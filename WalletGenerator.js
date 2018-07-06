7db0963d7d457dff5aeeecc9af3e4be1248609b4
Ventsislav Tsochev <ventsi@limechain.tech>
2018-07-06 10:33:38 +0300

    git ignore

--- WalletGenerator.js
+++ WalletGenerator.js
@@ -1,7 +1,5 @@
 const ethers = require('ethers');
-
-let counter = 1;
-
+const log = require('log-to-file');
 
 async function goGoEthLuck() {
     console.log("Start");
@@ -10,16 +8,19 @@ async function goGoEthLuck() {
     let provider = providers.getDefaultProvider('mainnet');
 
     while (true) {
-        counter++;
-        let wallet = ethers.Wallet.createRandom();
-        let balance = await provider.getBalance(wallet.address);
+        try {
+            let wallet = ethers.Wallet.createRandom();
+            let balance = await provider.getBalance(wallet.address);
 
-        if (balance.toString() !== "0") {
-            console.log("Cash Found: - " + balance.toString());
-            console.log(wallet);
-        }
-        if (counter % 10000 === 1) {
-            console.log("Damn, " + (counter - 1) + " wallets was searched");
+            if (balance.toString() !== "0") {
+                log("Cash Found: - " + balance.toString());
+                log(wallet.address);
+                log(wallet.privateKey);
+                console.log("Check it! ;)");
+            }
+        } catch (e) {
+            log(e);
+            goGoEthLuck();
         }
     }
 }
